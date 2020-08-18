8fa2315198e786ac256378165066e78084a7cc17
StarTux <ths.loy@googlemail.com>
2020-08-18 23:14:35 +0200

    Json

---
+++ src/main/java/com/cavetale/core/util/Json.java
@@ -0,0 +1,77 @@
+package com.cavetale.core.util;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.function.Supplier;
+import lombok.RequiredArgsConstructor;
+import org.bukkit.plugin.java.JavaPlugin;
+
+@RequiredArgsConstructor
+public final class Json {
+    public static final Gson GSON = new GsonBuilder().disableHtmlEscaping().create();
+    public static final Gson PRETTY = new GsonBuilder().disableHtmlEscaping().setPrettyPrinting().create();
+    private final JavaPlugin plugin;
+
+    public static <T> T load(final File file, Class<T> type, Supplier<T> dfl) {
+        if (!file.exists()) {
+            return dfl.get();
+        }
+        try (FileReader fr = new FileReader(file)) {
+            return GSON.fromJson(fr, type);
+        } catch (FileNotFoundException fnfr) {
+            return dfl.get();
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Loading " + file, ioe);
+        }
+    }
+
+    public static <T> T load(final File file, Class<T> type) {
+        return load(file, type, () -> null);
+    }
+
+    public static void save(final File file, Object obj, boolean pretty) {
+        try (FileWriter fw = new FileWriter(file)) {
+            Gson gs = pretty ? PRETTY : GSON;
+            gs.toJson(obj, fw);
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Saving " + file, ioe);
+        }
+    }
+
+    public static void save(final File file, Object obj) {
+        save(file, obj, false);
+    }
+
+    public static String serialize(Object obj) {
+        return GSON.toJson(obj);
+    }
+
+    public static <T> T deserialize(String json, Class<T> type) {
+        return deserialize(json, type, () -> null);
+    }
+
+    public static <T> T deserialize(String json, Class<T> type, Supplier<T> dfl) {
+        try {
+            return GSON.fromJson(json, type);
+        } catch (Exception e) {
+            System.err.println("Input: " + json);
+            e.printStackTrace();
+            return dfl.get();
+        }
+    }
+
+    public <T> T load(String path, Class<T> type, Supplier<T> dfl) {
+        File file = new File(plugin.getDataFolder(), path);
+        return load(file, type, dfl);
+    }
+
+    public void save(String path, Object obj, boolean pretty) {
+        File file = new File(plugin.getDataFolder(), path);
+        save(file, obj, pretty);
+    }
+}
