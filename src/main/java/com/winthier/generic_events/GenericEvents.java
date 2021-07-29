1df90b387efb11f4658852840a489a26a7335bbe
StarTux <ths.loy@googlemail.com>
2021-07-29 11:37:13 +0200

    Merge legacy classes

---
+++ src/main/java/com/winthier/generic_events/GenericEvents.java
@@ -0,0 +1,127 @@
+package com.winthier.generic_events;
+
+import java.util.UUID;
+import lombok.NonNull;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.Plugin;
+
+@Deprecated
+public final class GenericEvents {
+    private GenericEvents() { }
+
+    @Deprecated
+    public static boolean playerCanBuild(Player player, Block block) {
+        if (player.isOp()) return true;
+        PlayerCanBuildEvent event = new PlayerCanBuildEvent(player, block);
+        Bukkit.getPluginManager().callEvent(event);
+        if (event.isCancelled()) return false;
+        return com.cavetale.core.event.block.PlayerCanBuildEvent.call(player, block);
+    }
+
+    @Deprecated
+    public static boolean playerCanDamageEntity(Player player, Entity entity) {
+        if (player.isOp()) return true;
+        PlayerCanDamageEntityEvent event = new PlayerCanDamageEntityEvent(player, entity);
+        Bukkit.getServer().getPluginManager().callEvent(event);
+        return !event.isCancelled();
+    }
+
+    @Deprecated
+    public static boolean playerCanTeleport(Player player, Location location) {
+        PlayerCanTeleportEvent event = new PlayerCanTeleportEvent(player, location);
+        Bukkit.getServer().getPluginManager().callEvent(event);
+        return !event.isCancelled();
+    }
+
+    // Item
+
+    @Deprecated
+    public static String getItemName(ItemStack item) {
+        ItemNameEvent event = new ItemNameEvent(item);
+        Bukkit.getServer().getPluginManager().callEvent(event);
+        return event.getItemName();
+    }
+
+    // Money
+
+    @Deprecated
+    public static double getPlayerBalance(UUID uuid) {
+        PlayerBalanceEvent event = new PlayerBalanceEvent(uuid);
+        Bukkit.getServer().getPluginManager().callEvent(event);
+        return event.getBalance();
+    }
+
+    @Deprecated
+    public static boolean givePlayerMoney(UUID uuid,
+                                          double balance,
+                                          Plugin issuingPlugin,
+                                          String comment) {
+        if (Double.isNaN(balance)) {
+            throw new IllegalArgumentException("Balance cannot be NaN");
+        }
+        if (Double.isInfinite(balance)) {
+            throw new IllegalArgumentException("Balance cannot be infinite");
+        }
+        if (balance < 0) throw new IllegalArgumentException("Balance cannot be negative");
+        GivePlayerMoneyEvent event = new GivePlayerMoneyEvent(uuid, balance,
+                                                              issuingPlugin, comment);
+        Bukkit.getServer().getPluginManager().callEvent(event);
+        return event.isSuccessful();
+    }
+
+    @Deprecated
+    public static boolean takePlayerMoney(UUID uuid,
+                                          double balance,
+                                          Plugin issuingPlugin,
+                                          String comment) {
+        if (Double.isNaN(balance)) {
+            throw new IllegalArgumentException("Balance cannot be NaN");
+        }
+        if (Double.isInfinite(balance)) {
+            throw new IllegalArgumentException("Balance cannot be infinite");
+        }
+        if (balance < 0) throw new IllegalArgumentException("Balance cannot be negative");
+        TakePlayerMoneyEvent event = new TakePlayerMoneyEvent(uuid, balance,
+                                                              issuingPlugin, comment);
+        Bukkit.getServer().getPluginManager().callEvent(event);
+        return event.isSuccessful();
+    }
+
+    @Deprecated
+    public static String formatMoney(double money) {
+        FormatMoneyEvent event = new FormatMoneyEvent(money);
+        Bukkit.getServer().getPluginManager().callEvent(event);
+        if (event.getFormat() != null) return event.getFormat();
+        return "" + money;
+    }
+
+    // Permission
+
+    @Deprecated
+    public static boolean playerHasPermission(@NonNull UUID playerId, @NonNull String permission) {
+        PlayerHasPermissionEvent event = new PlayerHasPermissionEvent(playerId, permission);
+        Bukkit.getServer().getPluginManager().callEvent(event);
+        return event.isPermitted();
+    }
+
+    // Cache
+
+    @Deprecated
+    public static String cachedPlayerName(@NonNull UUID playerId) {
+        PlayerCacheEvent event = new PlayerCacheEvent(playerId);
+        Bukkit.getServer().getPluginManager().callEvent(event);
+        return event.getName();
+    }
+
+    @Deprecated
+    public static UUID cachedPlayerUuid(@NonNull String playerName) {
+        PlayerCacheEvent event = new PlayerCacheEvent(playerName);
+        Bukkit.getServer().getPluginManager().callEvent(event);
+        return event.getUniqueId();
+    }
+}
---
+++ src/main/java/com/winthier/generic_events/ItemNameEvent.java
@@ -0,0 +1,22 @@
+package com.winthier.generic_events;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.bukkit.inventory.ItemStack;
+
+@Deprecated
+@Getter
+@RequiredArgsConstructor
+public final class ItemNameEvent extends Event {
+    private final ItemStack item;
+    @Setter private String itemName;
+
+    // Event Stuff
+    @Getter private static HandlerList handlerList = new HandlerList();
+    @Override public HandlerList getHandlers() {
+        return handlerList;
+    }
+}
