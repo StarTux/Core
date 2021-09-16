b308cada086857d15f8226668bd0fbd7271e9098
StarTux <ths.loy@googlemail.com>
2021-09-16 13:11:39 +0200

    raid_start

--- src/main/java/com/cavetale/core/event/player/PluginPlayerEvent.java
+++ src/main/java/com/cavetale/core/event/player/PluginPlayerEvent.java
@@ -162,6 +162,7 @@ public final class PluginPlayerEvent extends Event implements Cancellable {
         LINK_PORTAL_TRAVEL,
         KIT_OPEN,
         INTERACT_NPC,
+        RAID_START,
         RAID_VICTORY,
         MINIGAME_PLAY_GAME,
         MINIGAME_WIN_GAME,
