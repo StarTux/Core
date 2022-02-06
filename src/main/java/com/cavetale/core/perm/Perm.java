822c5aae4f1100ccf748f10bbb9869638227b91c
StarTux <ths.loy@googlemail.com>
2022-02-06 14:12:13 +0100

    New permissions API

---
+++ src/main/java/com/cavetale/core/perm/Perm.java
@@ -0,0 +1,41 @@
+package com.cavetale.core.perm;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.UUID;
+
+public interface Perm {
+    default void register() {
+        Holder.perm = this;
+    }
+
+    default void unregister() {
+        Holder.perm = null;
+    }
+
+    static Perm get() {
+        return Holder.perm;
+    }
+
+    boolean has(UUID uuid, String permission);
+
+    boolean isInGroup(UUID uuid, String groupName);
+
+    Collection<String> getGroups(UUID uuid);
+
+    Collection<String> getAllGroups(UUID uuid);
+
+    Map<String, Boolean> getPerms(UUID uuid);
+
+    boolean removeGroup(UUID uuid, String group);
+
+    boolean addGroup(UUID uuid, String group);
+
+    boolean replaceGroup(UUID uuid, String oldGroup, String newGroup);
+}
+
+final class Holder {
+    private Holder() { };
+
+    protected static Perm perm;
+}
