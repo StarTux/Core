a2e1a0212f36e04ec6d9cbaaf2f6f2759355e175
StarTux <ths.loy@googlemail.com>
2022-07-03 10:57:34 +0200

    More completers and parsers

--- src/main/java/com/cavetale/core/command/CommandArgCompleter.java
+++ src/main/java/com/cavetale/core/command/CommandArgCompleter.java
@@ -173,16 +173,7 @@ public interface CommandArgCompleter {
         };
     }
 
-    CommandArgCompleter LONG = new CommandArgCompleter() {
-            @Override
-            public List<String> complete(CommandContext context, CommandNode node, String arg) {
-                try {
-                    return List.of("" + Long.parseLong(arg));
-                } catch (NumberFormatException nfe) {
-                    return List.of();
-                }
-            }
-        };
+    CommandArgCompleter LONG = longs(l -> true);
 
     static CommandArgCompleter longs(final LongPredicate validator) {
         return new CommandArgCompleter() {
@@ -200,16 +191,7 @@ public interface CommandArgCompleter {
         };
     }
 
-    CommandArgCompleter DOUBLE = new CommandArgCompleter() {
-            @Override
-            public List<String> complete(CommandContext context, CommandNode node, String arg) {
-                try {
-                    return List.of("" + Double.parseDouble(arg));
-                } catch (NumberFormatException nfe) {
-                    return List.of();
-                }
-            }
-        };
+    CommandArgCompleter DOUBLE = doubles(d -> true);
 
     static CommandArgCompleter doubles(final DoublePredicate validator) {
         return new CommandArgCompleter() {
@@ -227,6 +209,19 @@ public interface CommandArgCompleter {
         };
     }
 
+    CommandArgCompleter BOOLEAN = new CommandArgCompleter() {
+            @Override
+            public List<String> complete(CommandContext context, CommandNode node, String arg) {
+                List<String> result = new ArrayList<>(2);
+                for (String it : List.of("true", "false")) {
+                    if (it.contains(arg.toLowerCase())) {
+                        result.add(it);
+                    }
+                }
+                return result;
+            }
+        };
+
     static int requireInt(String arg, IntPredicate validator) {
         final int input;
         try {
@@ -242,5 +237,61 @@ public interface CommandArgCompleter {
 
     static int requireInt(String arg) {
         return requireInt(arg, i -> true);
+    }
+
+    static long requireLong(String arg, LongPredicate validator) {
+        final long input;
+        try {
+            input = Long.parseLong(arg);
+        } catch (NumberFormatException nfe) {
+            throw new CommandWarn("Number expected: " + arg);
+        }
+        if (!validator.test(input)) {
+            throw new CommandWarn("Invalid number: " + arg);
+        }
+        return input;
+    }
+
+    static long requireLong(String arg) {
+        return requireLong(arg, i -> true);
+    }
+
+    static double requireDouble(String arg, DoublePredicate validator) {
+        final double input;
+        try {
+            input = Double.parseDouble(arg);
+        } catch (NumberFormatException nfe) {
+            throw new CommandWarn("Number expected: " + arg);
+        }
+        if (!validator.test(input)) {
+            throw new CommandWarn("Invalid number: " + arg);
+        }
+        return input;
+    }
+
+    static double requireDouble(String arg) {
+        return requireDouble(arg, i -> true);
+    }
+
+    static boolean requireBoolean(String arg) {
+        switch (arg.toLowerCase()) {
+        case "true":
+        case "on":
+        case "yes":
+            return true;
+        case "false":
+        case "off":
+        case "no":
+            return false;
+        default: throw new CommandWarn("True or false expected: " + arg);
+        }
+    }
+
+    static <E extends Enum<E>> E requireEnum(Class<E> enumClass, String arg) {
+        try {
+            return Enum.valueOf(enumClass, arg.toUpperCase());
+        } catch (IllegalArgumentException iae) {
+            throw new CommandWarn(enumClass.getSimpleName() + " expected: " + arg);
+        }
     }
 }
