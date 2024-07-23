fe81d54373230e460a02322614f00cfed50d1eff
StarTux <ths.loy@gmail.com>
2024-07-23 18:43:29 +0200

    Make structure an interface

--- src/main/java/com/cavetale/core/structure/Structure.java
+++ src/main/java/com/cavetale/core/structure/Structure.java
@@ -1,11 +1,14 @@
 package com.cavetale.core.structure;
 
 import com.cavetale.core.struct.Cuboid;
-import lombok.Value;
 import org.bukkit.NamespacedKey;
 
-@Value
-public final class Structure {
-    private final NamespacedKey key;
-    private final Cuboid boundingBox;
+public interface Structure {
+    NamespacedKey getKey();
+
+    Cuboid getBoundingBox();
+
+    boolean isDiscovered();
+
+    void setDiscovered(boolean value);
 }
