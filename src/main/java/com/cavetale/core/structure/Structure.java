85f3bb7366cf927b0a56534dcfe73497bedd5f5c
StarTux <ths.loy@gmail.com>
2024-07-31 09:45:14 +0200

    Add id and world name getters

--- src/main/java/com/cavetale/core/structure/Structure.java
+++ src/main/java/com/cavetale/core/structure/Structure.java
@@ -1,9 +1,19 @@
 package com.cavetale.core.structure;
 
 import com.cavetale.core.struct.Cuboid;
+import org.bukkit.Bukkit;
 import org.bukkit.NamespacedKey;
+import org.bukkit.World;
 
 public interface Structure {
+    int getInternalId();
+
+    String getWorldName();
+
+    default World getWorld() {
+        return Bukkit.getWorld(getWorldName());
+    }
+
     NamespacedKey getKey();
 
     Cuboid getBoundingBox();
