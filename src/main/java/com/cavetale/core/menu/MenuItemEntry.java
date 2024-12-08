3857e57034c924cb17fffd8240b85a6ae388d2ae
StarTux <ths.loy@gmail.com>
2024-12-08 11:34:35 +0100

    Highlight color and more sorting options

--- src/main/java/com/cavetale/core/menu/MenuItemEntry.java
+++ src/main/java/com/cavetale/core/menu/MenuItemEntry.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import lombok.NonNull;
 import lombok.Value;
 import lombok.experimental.Accessors;
+import net.kyori.adventure.text.format.TextColor;
 import org.bukkit.inventory.ItemStack;
 
 /**
@@ -16,16 +17,15 @@ public final class MenuItemEntry implements Comparable<MenuItemEntry> {
     @NonNull private final Priority priority;
     @NonNull private final String key; // e.g. "money"
     @NonNull private final ItemStack icon;
+    @NonNull private String sortingName;
     private final String command;
+    private final TextColor highlightColor;
 
     public enum Priority {
         SERVER,
         HOTBAR,
-        HIGHEST,
-        HIGH,
+        NOTIFICATION,
         REGULAR,
-        LOW,
-        LOWEST,
         ;
 
         public int getValue() {
@@ -39,16 +39,28 @@ public final class MenuItemEntry implements Comparable<MenuItemEntry> {
         private Priority priority = Priority.REGULAR;
         private String key;
         private ItemStack icon;
+        private String sortingName;
         private String command;
+        private TextColor highlightColor;
 
         public MenuItemEntry build() {
-            return new MenuItemEntry(priority, key, icon, command);
+            if (priority == null) throw new NullPointerException("priority cannot be null");
+            if (key == null) throw new NullPointerException("key cannot be null");
+            if (icon == null) throw new NullPointerException("icon cannot be null");
+            if (sortingName == null) {
+                sortingName = key.contains(":")
+                    ? key.split(":", 2)[1]
+                    : key;
+            }
+            return new MenuItemEntry(priority, key, icon, sortingName, command, highlightColor);
         }
     }
 
     @Override
     public int compareTo(MenuItemEntry other) {
-        return Integer.compare(priority.getValue(), other.priority.getValue());
+        final int prio = Integer.compare(priority.getValue(), other.priority.getValue());
+        if (prio != 0) return prio;
+        return sortingName.compareToIgnoreCase(other.sortingName);
     }
 
     public static Builder builder() {
@@ -57,5 +69,9 @@ public final class MenuItemEntry implements Comparable<MenuItemEntry> {
 
     public boolean hasCommand() {
         return command != null;
+    }
+
+    public boolean hasHighlightColor() {
+        return highlightColor != null;
     }
 }
