5c2484fa31ee983a4a09f741837206f788b9595f
StarTux <ths.loy@gmail.com>
2025-04-02 09:50:51 +0200

    Store the EntityDeathEvent

--- src/main/java/com/cavetale/core/event/skills/SkillsMobKillRewardEvent.java
+++ src/main/java/com/cavetale/core/event/skills/SkillsMobKillRewardEvent.java
@@ -7,31 +7,24 @@ import org.bukkit.entity.AbstractArrow;
 import org.bukkit.entity.Mob;
 import org.bukkit.entity.Player;
 import org.bukkit.event.HandlerList;
+import org.bukkit.event.entity.EntityDeathEvent;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
 public final class SkillsMobKillRewardEvent extends SkillsRewardEvent {
     private final AbstractArrow arrow;
     private final Mob mob;
+    private final EntityDeathEvent entityDeathEvent;
 
-    public SkillsMobKillRewardEvent(final Player player, final AbstractArrow arrow, final Mob mob) {
-        super(player);
-        this.arrow = arrow;
-        this.mob = mob;
-    }
-
-    public SkillsMobKillRewardEvent(final Player player, final Mob mob) {
-        this(player, null, mob);
-    }
-
-    public SkillsMobKillRewardEvent(final Player player, final AbstractArrow arrow, final Mob mob, final int skillPoints, final double money, final int exp) {
+    public SkillsMobKillRewardEvent(final Player player, final AbstractArrow arrow, final Mob mob, final int skillPoints, final double money, final int exp, final EntityDeathEvent entityDeathEvent) {
         super(player, skillPoints, money, exp);
         this.arrow = arrow;
         this.mob = mob;
+        this.entityDeathEvent = entityDeathEvent;
     }
 
-    public SkillsMobKillRewardEvent(final Player player, final Mob mob, final int skillPoints, final double money, final int exp) {
-        this(player, null, mob, skillPoints, money, exp);
+    public SkillsMobKillRewardEvent(final Player player, final Mob mob, final int skillPoints, final double money, final int exp, final EntityDeathEvent entityDeathEvent) {
+        this(player, null, mob, skillPoints, money, exp, entityDeathEvent);
     }
 
     public boolean isMelee() {
