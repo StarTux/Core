d30afcbfc860ae91118d7d36d266ad56cde6c278
StarTux <ths.loy@gmail.com>
2026-01-15 01:48:26 +0100

    Implement out item pickup policies in the convenience method

--- src/main/java/com/cavetale/core/event/item/PlayerReceiveItemsEvent.java
+++ src/main/java/com/cavetale/core/event/item/PlayerReceiveItemsEvent.java
@@ -16,10 +16,13 @@ import org.bukkit.inventory.ItemStack;
 /**
  * Called when a player is to receive a number of items.  Clients
  * should use this opportunity to remove the items to place them into
- * their storage, such as MassStorage or Stash.
+ * their storage.
  *
- * Stored items must be removed from the list, or better, reduced in
- * amount via the subtract method.
+ * The expected responder to this event is MassStorage.
+ *
+ * All items that were removed must have their amount updated.
+ * Editing the list is not enough because the update will not carry
+ * over to a backing inventory.
  *
  * Remaining items will likely be dropped.  There is a utility method
  * to do so.
@@ -43,20 +46,36 @@ public final class PlayerReceiveItemsEvent extends Event {
         this.player = player;
         this.items = new ArrayList<>();
         for (ItemStack item : inventory) {
-            if (item == null || item.getType().isAir()) continue;
+            if (item == null || item.isEmpty()) continue;
             items.add(item);
         }
     }
 
     /**
-     * All-round carefree convenience method.
-     * Call the event with all the items, then drop the remaining items.
+     * All-round carefree convenience method which implements our
+     * desired policy.
+     *
+     * First, we try to stack the new items with existing ones in the
+     * player inventory.
+     *
+     * Then, we try to put them in empty hotbar slots, if the player
+     * does not already have such an item in their inventory.
+     *
+     * Then, the event is called. Here, we expect our plugins
+     * (especially MassStorage) to do their magic.
+     *
+     * Then, we try to add all the remaining items to the player
+     * inventory.
+     *
+     * Finally, as a last resort, items are dropped to the ground near
+     * the player.
      */
     public static void receiveItems(Player player, List<ItemStack> items) {
         PlayerReceiveItemsEvent event = new PlayerReceiveItemsEvent(player, items);
-        event.giveItems();
-        if (event.isEmpty()) return;
+        event.stackWithPlayerInventory();
+        event.putInEmptyHotbar();
         event.callEvent();
+        event.giveItems();
         event.dropItems();
     }
 
@@ -65,17 +84,19 @@ public final class PlayerReceiveItemsEvent extends Event {
      * Call the event with the inventory, then drop the remaining items.
      */
     public static void receiveInventory(Player player, Inventory inventory) {
-        PlayerReceiveItemsEvent event = new PlayerReceiveItemsEvent(player, inventory);
-        event.giveItems();
-        if (event.isEmpty()) return;
-        event.callEvent();
-        event.dropItems();
+        final List<ItemStack> items = new ArrayList<>();
+        for (ItemStack item : inventory) {
+            if (item == null || item.isEmpty()) continue;
+            items.add(item);
+        }
+        receiveItems(player, items);
     }
 
     /**
      * Attempt to add the items to the player inventory.  This will
      * remove or nullify items from the list, but will never drop
      * anything.
+     *
      * This could be used before calling the event to ensure that
      * items are preferrably added to the inventory, rather than
      * absorbed by MS.
@@ -83,49 +104,88 @@ public final class PlayerReceiveItemsEvent extends Event {
     public void giveItems() {
         for (int i = 0; i < items.size(); i += 1) {
             ItemStack itemStack = items.get(i);
-            if (itemStack == null || itemStack.getType().isAir()) continue;
+            if (itemStack == null || itemStack.isEmpty()) continue;
             Collection<ItemStack> remainder = player.getInventory().addItem(itemStack).values();
-            items.set(i, !remainder.isEmpty() ? remainder.iterator().next() : null);
+            final ItemStack retour = !remainder.isEmpty() ? remainder.iterator().next() : null;
+            itemStack.setAmount(retour != null ? retour.getAmount() : 0);
+        }
+    }
+
+    /**
+     * Attempt to stack all items with already existing items in the
+     * player inventory.
+     */
+    public void stackWithPlayerInventory() {
+        for (ItemStack itemStack : items) {
+            if (itemStack == null || itemStack.isEmpty()) continue;
+            for (int i = 0; i < 40 && itemStack.getAmount() > 0; i += 1) {
+                if (i >= 36 && i <= 39) continue; // armor slots
+                final ItemStack slot = player.getInventory().getItem(i);
+                if (slot == null || slot.isEmpty()) {
+                    continue;
+                }
+                if (!itemStack.isSimilar(slot)) continue;
+                final int stacking = Math.min(itemStack.getAmount(), slot.getMaxStackSize() - slot.getAmount());
+                if (stacking <= 0) continue;
+                itemStack.subtract(stacking);
+                slot.add(stacking);
+            }
+        }
+    }
+
+    /**
+     * Attempt to place all items in the empty hotbar slot, provided
+     * that item does not already exist in the player inventory.
+     */
+    public void putInEmptyHotbar() {
+        for (ItemStack itemStack : items) {
+            if (itemStack == null || itemStack.isEmpty()) continue;
+            if (player.getInventory().containsAtLeast(itemStack, 1)) continue;
+            for (int i = 0; i < 9; i += 1) {
+                final ItemStack slot = player.getInventory().getItem(i);
+                if (slot != null && !slot.isEmpty()) continue;
+                player.getInventory().setItem(i, itemStack.clone());
+                itemStack.setAmount(0);
+            }
         }
     }
 
     /**
      * Drop all remaining items.  Items will be dropped with
-     * ownership, invulerable, not able to be picked up by mobs, with
-     * PlayerDropItemEvent.  The latter will cancel its cancellation
+     * ownership, invulerable, not able to be picked up by mobs.
+     * PlayerDropItemEvent.  The latter will ignore its cancellation
      * state.
-     * Before dropping any item, attempt to add it to the player
-     * inventory.
      */
     public void dropItems() {
-        for (ItemStack itemStack : items) {
-            if (itemStack == null || itemStack.getType().isAir()) continue;
-            for (ItemStack drop : player.getInventory().addItem(itemStack).values()) {
-                if (drop == null || drop.getType().isAir()) continue;
-                Item item = player.getWorld().dropItem(player.getLocation(), drop);
-                item.setOwner(player.getUniqueId());
-                item.setCanMobPickup(false);
-                item.setOwner(player.getUniqueId());
-                item.setPickupDelay(0);
-                item.setInvulnerable(true);
-                new PlayerDropItemEvent(player, item).callEvent();
-            }
+        for (int i = 0; i < items.size(); i += 1) {
+            final ItemStack itemStack = items.get(i);
+            if (itemStack == null || itemStack.isEmpty()) continue;
+            Item item = player.getWorld().dropItem(player.getLocation(), itemStack.clone());
+            itemStack.setAmount(0);
+            item.setOwner(player.getUniqueId());
+            item.setCanMobPickup(false);
+            item.setOwner(player.getUniqueId());
+            item.setPickupDelay(0);
+            item.setInvulnerable(true);
+            new PlayerDropItemEvent(player, item).callEvent();
         }
-        items.clear();
     }
 
     /**
-     * Get the item list.  The returned list will be a modifiable
-     * ArrayList.
-     * Beware: Any entry may be null or an AIR item.
+     * Get the unmodifiable item list.
      */
     public List<ItemStack> getItems() {
-        return items;
+        final List<ItemStack> result = new ArrayList<>();
+        for (ItemStack itemStack : items) {
+            if (itemStack == null || itemStack.isEmpty()) continue;
+            result.add(itemStack);
+        }
+        return result;
     }
 
     public boolean isEmpty() {
         for (ItemStack item : items) {
-            if (item != null && !item.getType().isAir() && item.getAmount() > 0) return false;
+            if (item != null && !item.isEmpty()) return false;
         }
         return true;
     }
